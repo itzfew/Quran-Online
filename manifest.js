05019a43552b7ea5de495879919a37b0e279e52d
Itzfew <113174780+itzfew@users.noreply.github.com>
2024-11-06 17:06:20 +0530

    Update and rename manifest.json to manifest.js

---
+++ manifest.js
@@ -0,0 +1,21 @@
+{
+  "name": "Quran Kareem",
+  "short_name": "Quran Kareem",
+  "description": "A comprehensive and beautiful Quran Kareem app.",
+  "start_url": "/index.html",
+  "display": "standalone",
+  "background_color": "#ffffff",
+  "theme_color": "#4CAF50",
+  "icons": [
+    {
+      "src": "https://quran-hadith.pages.dev/source/pic.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "https://quran-hadith.pages.dev/source/pic.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
