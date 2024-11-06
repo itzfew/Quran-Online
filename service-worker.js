93d0b894e5b16a06d1742df7a046eadd2c59150f
Itzfew <113174780+itzfew@users.noreply.github.com>
2024-11-06 17:07:25 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,47 +1,39 @@
-const CACHE_NAME = 'Quran-Online-cache-v1';
-const URLS_TO_CACHE = [
-    '/',
-    '/index.html',
-    '/post-template.html',
-    '/blog.html',
-    '/data/posts.json',
-    '/css/styles.css',
-    '/js/main.js',
-    '/js/post.js',
-    '/icons/192.png',
-    '/icons/512.png',
+const CACHE_NAME = 'quran-kareem-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/script.js',
+  'https://quran-hadith.pages.dev/source/pic.png',
 ];
 
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                return cache.addAll(URLS_TO_CACHE);
-            })
-    );
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                return response || fetch(event.request);
-            })
-    );
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
 });
 
-self.addEventListener('activate', event => {
-    const cacheWhitelist = [CACHE_NAME];
-
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (!cacheWhitelist.includes(cacheName)) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
         })
-    );
+      );
+    })
+  );
 });
