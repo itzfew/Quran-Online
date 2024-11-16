25063f8bc143fa295cc5fdd096c3b3284670f68e
Itzfew <113174780+itzfew@users.noreply.github.com>
2024-11-16 23:58:07 +0530

    Add files via upload

---
+++ sevice-worker.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'pwa-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/surah.html',
+    '/hadith.html',
+    'Quran_kareem_logo.png',
+    'Quran_kareem_logo.png'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                return response || fetch(event.request);
+            })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
